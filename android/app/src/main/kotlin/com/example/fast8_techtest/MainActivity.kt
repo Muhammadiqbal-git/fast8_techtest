b593af7003ad7527b567cab0b0014b28fdbd2baf
iqbal <iqbal.dev.code@gmail.com>
2024-09-14 14:34:12 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/fast8_techtest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fast8_techtest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/custom_color.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const Color primaryOrange = Color(0XFFF8C20A);
